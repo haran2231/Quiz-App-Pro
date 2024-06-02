c95195c51d10631b681c3e9f4a0a80ecf92d2429
haran2231 <haran2231@gmail.com>
2024-06-02 23:17:50 +0530

    updated

--- script.js
+++ script.js
@@ -1,169 +1,152 @@
-const ttlQuestionCount = 10;
-
-const userData = {};
-
-let allQuestions = [];
 let selectedQuestions = [];
-
-const userSelecetedAnswer = [];
-
+let allQuestions = [];
+let ttlQuestionCount = 10;
 let currentQuestionIdx = 0;
-
 let mark = 0;
+let userData = {}
 
-let ttlTime = 600;
+//dom load
+document.addEventListener("DOMContentLoaded", () => {
+    loadPage("Landing.html");
+    questions();
 
-const selectRandomQuestion = () => {
-  let i = 0;
-  let shuffledQuestion = allQuestions.sort(() => {
-    return 5 - Math.floor(Math.random() * 10);
-  });
+    // console.log(selectedQuestions);
 
-  selectedQuestions = shuffledQuestion.slice(0, ttlQuestionCount);
-};
+})
 
-const showQuestion = () => {
-  const currentQuestion = selectedQuestions[currentQuestionIdx];
-
-  const questionContainer = document.getElementById("questionContainer");
-
-  questionContainer.innerHTML = `
-        <h4>Question ${currentQuestionIdx + 1}: ${currentQuestion.question}</h4>
-        ${currentQuestion.answers
-          .map((ans, idx) => {
-            return `
-          <div>
-          <label>
-          <input type="radio" name="answer" value="${idx}">${ans}</label></div>`;
-          })
-          .join(" ")}
-    `;
-};
-
-const showAnswer = () => {
-  const answerContainer = document.getElementById("answerContainer");
-
-  //   const firstQuestion = selectedQuestions[0];
-
-  answerContainer.innerHTML = selectedQuestions
-    .map(
-      (question, qNo) => `
-        <div>
-            <p>Question ${qNo + 1}: ${question.question}</p>
-
-            ${question.answers
-              .map((ans, ansIdx) => {
-                let color = "black";
-
-                if (question.correct == ansIdx) {
-                  color = "green";
-                } else if (userSelecetedAnswer[qNo] == ansIdx) {
-                  color = "red";
-                }
-
-                return `
-              <p style="color: ${color}">${ans}</p>
-                `;
-              })
-              .join(" ")}
-        </div>
-    `
-    )
-    .join(" ");
-};
-
-const formatTimer = (duration) => {
-  let seconds = Math.floor(duration % 60);
-  let minutes = Math.floor((duration / 60) % 60);
-  let hours = Math.floor(duration / (60 * 60));
-
-  return `${String(hours).padStart(2, 0)} : ${String(minutes).padStart(
-    2,
-    0
-  )} : ${String(seconds).padStart(2, 0)}`;
-};
-
-const showTimer = () => {
-  const timer = document.getElementById("timer");
-  timer.innerText = formatTimer(ttlTime);
-
-  let timerInterval = setInterval(() => {
-    ttlTime--;
-    console.log("ttlTime", ttlTime);
-    timer.innerText = formatTimer(ttlTime);
-    if (ttlTime == 0) {
-      clearInterval(timerInterval);
-      loadPage("Result.html");
-    }
-  }, 1000);
-};
+//initiasePage
 
 const initiasePage = (path) => {
-  if (path == "Landing.html") {
-    document.getElementById("userForm").addEventListener("submit", (e) => {
-      e.preventDefault();
+    if (path == "Landing.html") {
+        document.getElementById("userForm").addEventListener("submit", (e) => {
+            e.preventDefault();
+            userData.name = document.getElementById("name").value
+            userData.email = document.getElementById("email").value
+            // console.log(userData);
+            loadPage("Instruction.html")
+        })
+    } else if (path == "Instruction.html") {
+        document.getElementById("startQuiz").addEventListener("click", () => {
+            loadPage("Quiz.html")
+            // showQuestion()
+            // alert('hhh')
 
-      let userName = document.getElementById("name").value;
-      let userMail = document.getElementById("email").value;
+        })
+    } else if (path === "Quiz.html") {
+        showTimer()
+        showQuestion()
+        document.getElementById("next").addEventListener("click", () => {
+            // mark
+            let selectedAnswer = document.querySelector('input[name="answer"]:checked');
 
-      userData.name = userName;
-      userData.email = userMail;
+            if(selectedAnswer.value == selectedQuestions[currentQuestionIdx].correct){
+                mark++
+                // console.log(mark);
+            }
 
-      loadPage("Instruction.html");
+            //next 
+            if (currentQuestionIdx < selectedQuestions.length - 1) {
+                currentQuestionIdx++;
+                showQuestion();
+            } else {
+                loadPage("Result.html")
+               
+            }
+        });
+
+        document.getElementById("previous").addEventListener("click", () => {
+            if (currentQuestionIdx < selectedQuestions.length - 1) {
+
+                if (currentQuestionIdx == 0) {
+                    alert("First Question")
+                } else {
+                    currentQuestionIdx--;
+                    // console.log(currentQuestionIdx);
+                    showQuestion();
+                }
+            }
+        })
+    }else if(path == "Result.html"){
+        if(mark < 3){
+            document.getElementById("details").innerHTML = `Hi, ${userData.name}.... Better luck next time We will send next test link to ${userData.email}`
+        }else{
+            document.getElementById("details").innerHTML = `Hi, ${userData.name}.... We will send next round details to ${userData.email}`
+        }
+        document.getElementById("mark").innerHTML = `Hey Your Score is ${mark}`
+    }
+}
+
+//fetch question from array object
+const questions = () => {
+    fetch("questions.json")
+        .then((data) => data.json())
+        .then((question) => {
+            // console.log(question);
+            allQuestions = question;
+            // console.log(allQuestions);
+            selectRandomQuestion();
+        })
+}
+
+// need to fetch random 10 questions
+const selectRandomQuestion = () => {
+    let i = 0;
+    let shuffledQuestion = allQuestions.sort(() => {
+        return 5 - Math.floor(Math.random() * 10);
     });
-  } else if (path == "Instruction.html") {
-    document.getElementById("startQuiz").addEventListener("click", (e) => {
-      loadPage("Quiz.html");
-    });
-  } else if (path == "Quiz.html") {
-    showTimer();
-    showQuestion();
 
-    document.getElementById("next").addEventListener("click", () => {
-      let selectedAnswer = document.querySelector(
-        'input[name="answer"]:checked'
-      );
-
-      if (
-        selectedAnswer.value == selectedQuestions[currentQuestionIdx].correct
-      ) {
-        mark++;
-      }
-
-      userSelecetedAnswer[currentQuestionIdx] = selectedAnswer.value;
-
-      currentQuestionIdx++;
-      if (currentQuestionIdx < ttlQuestionCount) showQuestion();
-      else loadPage("Result.html");
-    });
-  } else if (path == "Result.html") {
-    document.getElementById(
-      "mark"
-    ).textContent = `You're score is ${mark} / ${ttlQuestionCount}`;
-    showAnswer();
-  }
+    selectedQuestions = shuffledQuestion.slice(0, ttlQuestionCount);
+    //   console.log(selectedQuestions);
 };
+
+// show question in quiz page
+
+const showQuestion = () => {
+    const currentQuestion = selectedQuestions[currentQuestionIdx];
+
+    const questionContainer = document.getElementById("questionContainer");
+
+    questionContainer.innerHTML = `
+          <h4>Question ${currentQuestionIdx + 1}: ${currentQuestion.question}</h4>
+          ${currentQuestion.answers
+            .map((ans, idx) => {
+                return `
+            <div class="opt">
+            <label>
+            <input type="radio" name="answer" value="${idx}">${ans}</label></div>`;
+            })
+            .join(" ")}
+      `;
+};
+
+// timer
+function showTimer() {
+    const timerElement = document.getElementById('timer');
+    let remainingTime = 10 * 60; // 10 minutes in seconds
+
+    const interval = setInterval(() => {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+        timerElement.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+
+        if (remainingTime > 0) {
+            remainingTime--;
+        } else {
+            clearInterval(interval);
+            console.log('10 minutes are up!');
+        }
+    }, 1000);
+}
+
+//call pages
 
 const loadPage = (path) => {
-  fetch(`./page/${path}`)
-    .then((data) => data.text())
-    .then((html) => {
-      document.getElementById("app").innerHTML = html;
-      initiasePage(path);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+    fetch(`./page/${path}`)
+        .then((data) => data.text())
+        .then((html) => {
+            // console.log("gdgdg");
+            document.getElementById("app").innerHTML = html;
+            initiasePage(path);
+        });
 
-//Once the DOM loaded the only the script js need to start
-document.addEventListener("DOMContentLoaded", () => {
-  loadPage("Landing.html");
-  fetch("questions.json")
-    .then((data) => data.json())
-    .then((questions) => {
-      //   console.log("question", questions);
-      allQuestions = questions;
-      selectRandomQuestion();
-    });
-});
+}
